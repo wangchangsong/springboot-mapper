e4e8bb0387f68e2711dc3b827e8646dac1fe6abe
wangchangsong <3121885499@qq.com>
2017-11-26 18:36:45 +0800

    新建项目

---
+++ src/main/java/com/mapper/example/MapperApplication.java
@@ -0,0 +1,12 @@
+package com.mapper.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperApplication.class, args);
+	}
+}
